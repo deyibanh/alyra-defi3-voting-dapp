7ee75521cd53fed9623ca757d11a2221fe61f544
Dé Yi Banh <deyi.banh@gmail.com>
2021-11-29 21:21:12 +0100

    Updates

---
+++ precache-manifest.7bf7d8dd4b6d32b193724826d788cfae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007ddc99234083bb0a35fd260445b235",
+    "url": "/alyra-defi3-voting-dapp/index.html"
+  },
+  {
+    "revision": "38bf71b42b1ee18a0148",
+    "url": "/alyra-defi3-voting-dapp/static/css/2.6a23f8e0.chunk.css"
+  },
+  {
+    "revision": "2ea75e17cb5538427daa",
+    "url": "/alyra-defi3-voting-dapp/static/css/main.d8e6d7c2.chunk.css"
+  },
+  {
+    "revision": "38bf71b42b1ee18a0148",
+    "url": "/alyra-defi3-voting-dapp/static/js/2.37ffec63.chunk.js"
+  },
+  {
+    "revision": "2ea75e17cb5538427daa",
+    "url": "/alyra-defi3-voting-dapp/static/js/main.7d0ac561.chunk.js"
+  },
+  {
+    "revision": "224403b80d1e9ca1c594",
+    "url": "/alyra-defi3-voting-dapp/static/js/runtime-main.e69f4fc6.js"
+  }
+]);
